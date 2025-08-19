096309e2b5d2560b3b77496d025392f2610f5173
Catherine Daniel <catherine_deborah@bluerose-tech.com>
2025-08-19 11:29:07 +0530

    Updated routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+/*import 'dart:io';
 import 'dart:typed_data';
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -51,7 +51,7 @@ class _WebViewScreenState extends State<WebViewScreen> {
       final folderPath = '${tempDir.path}/www';
       await Directory(folderPath).create(recursive: true);
 
-      //await _copyAssetToFile('assets/www/index.html', '$folderPath/index.html');
+      await _copyAssetToFile('assets/www/index.html', '$folderPath/index.html');
 
       await _copyAssetToFile('assets/www/css', '$folderPath/css');
       await _copyAssetToFile('assets/www/js', '$folderPath/js');
@@ -151,4 +151,162 @@ class _WebViewScreenState extends State<WebViewScreen> {
             },
           ),
   );
+}*/
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Enable remote debugging if you connect via adb (optional)
+  if (Platform.isAndroid) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(true);
+  }
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebViewScreen(),
+    );
+  }
+}
+
+class WebViewScreen extends StatefulWidget {
+  const WebViewScreen({super.key});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  final InAppLocalhostServer _server = InAppLocalhostServer(
+    documentRoot: 'assets',
+    port: 8080,
+  );
+  InAppWebViewController? _controller;
+
+  final _startUrl = WebUri(
+    'http://localhost:8080/www/index.html',
+  ); // maps to assets/www/...
+
+  bool _starting = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _startServerAndLoad();
+  }
+
+  Future<void> _startServerAndLoad() async {
+    try {
+      // Start the embedded localhost web server that serves from /assets
+      await _server.start();
+    } catch (_) {
+      // If it was already running, ignore
+    }
+    setState(() => _starting = false);
+  }
+
+  @override
+  void dispose() {
+    _server.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_starting) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+
+    return WillPopScope(
+      onWillPop: () async {
+        // If the game gets stuck or user presses back: try to go back; if not, hard reload.
+        if (_controller != null) {
+          final canGoBack = await _controller!.canGoBack();
+          if (canGoBack) {
+            await _controller!.goBack();
+          } else {
+            await _controller!.reload();
+          }
+        }
+        return false;
+      },
+      child: Scaffold(
+        body: InAppWebView(
+          initialUrlRequest: URLRequest(url: _startUrl),
+          // If localhost fails for any reason, fall back to file:// asset path.
+          onReceivedError: (c, r, e) async {
+            await c.loadUrl(
+              urlRequest: URLRequest(
+                url: WebUri(
+                  'file:///android_asset/flutter_assets/assets/www/index.html',
+                ),
+              ),
+            );
+          },
+          initialSettings: InAppWebViewSettings(
+            javaScriptEnabled: true,
+            allowFileAccessFromFileURLs: true,
+            allowUniversalAccessFromFileURLs: true,
+            mediaPlaybackRequiresUserGesture: false,
+            domStorageEnabled: true,
+            databaseEnabled: true,
+            cacheEnabled: true,
+            mixedContentMode: MixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
+            useHybridComposition: true, // better stability on modern Android
+          ),
+          onWebViewCreated: (controller) => _controller = controller,
+          onConsoleMessage: (controller, msg) {
+            // See JS errors in `flutter logs`
+            debugPrint(
+              '[WEBVIEW] ${msg.messageLevel.toString()}: ${msg.message}',
+            );
+          },
+          onLoadStop: (controller, url) async {
+            // Defensive patch: if the game's JS throws on `*.unload()` being null
+            // after exiting to the menu, immediately hard-reload the page so the app
+            // never freezes on "screen 3".
+            await controller.evaluateJavascript(
+              source: r'''
+              (function() {
+                try {
+                  // Howler audio unlock on mobile (if present)
+                  if (window.Howler) { Howler.autoUnlock = true; }
+
+                  // Global error handler to catch the "unload of null" crash
+                  if (!window.__reloadOnUnloadCrash) {
+                    window.__reloadOnUnloadCrash = true;
+                    window.onerror = function(msg, src, line, col, err) {
+                      try {
+                        var m = (msg && msg.toString()) || '';
+                        if (m.indexOf('unload') !== -1 || m.indexOf('Cannot read') !== -1) {
+                          // Give WebView a tick, then hard reload
+                          setTimeout(function(){ location.reload(); }, 50);
+                          return true; // handled
+                        }
+                      } catch(_){}
+                      return false;
+                    };
+                  }
+
+                  // Also expose a manual panic-reload if you want to call from the game:
+                  window.__flutterReload = function(){ location.reload(); };
+                } catch (e) {}
+              })();
+            ''',
+            );
+          },
+        ),
+      ),
+    );
+  }
 }
